056a6bc253b93e61f167782dc7ad5513f84b2577
ajs224 <ajs224@cam.ac.uk>
2015-08-14 18:50:55 +0100

    Added cmake build scripts and tidied everything up

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/userspace/ajs224/C++/pbIterativeSolver
+# Build directory: /home/userspace/ajs224/C++/pbIterativeSolver/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
